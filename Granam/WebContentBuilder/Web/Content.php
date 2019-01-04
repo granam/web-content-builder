86d7beb36b277f0f83d2c66c8254d2f562d15159
Jaroslav Týc <mail@jaroslavtyc.com>
2019-01-04 16:30:24 +0100

    Removed redirection support

--- Granam/WebContentBuilder/Web/Content.php
+++ Granam/WebContentBuilder/Web/Content.php
@@ -5,7 +5,6 @@ namespace Granam\WebContentBuilder\Web;
 
 use Granam\WebContentBuilder\HtmlDocument;
 use Granam\WebContentBuilder\HtmlHelper;
-use Granam\WebContentBuilder\Redirect;
 use Granam\Strict\Object\StrictObject;
 
 class Content extends StrictObject
@@ -16,15 +15,12 @@ class Content extends StrictObject
     private $head;
     /** @var Body */
     private $body;
-    /** @var Redirect|null */
-    private $redirect;
 
-    public function __construct(HtmlHelper $htmlHelper, Head $head, Body $body, ?Redirect $redirect)
+    public function __construct(HtmlHelper $htmlHelper, Head $head, Body $body)
     {
         $this->htmlHelper = $htmlHelper;
         $this->head = $head;
         $this->body = $body;
-        $this->redirect = $redirect;
     }
 
     public function __toString()
@@ -36,10 +32,8 @@ class Content extends StrictObject
     {
         $content = $this->composeContent();
         $htmlDocument = $this->buildHtmlDocument($content);
-        $updatedContent = $htmlDocument->saveHTML();
 
-        // has to be AFTER cache as we do not want to cache it
-        return $this->injectRedirectIfAny($updatedContent);
+        return $htmlDocument->saveHTML();
     }
 
     private function buildHtmlDocument(string $content): HtmlDocument
@@ -71,25 +65,5 @@ class Content extends StrictObject
 </body>
 </html>
 HTML;
-    }
-
-    private function injectRedirectIfAny(string $content): string
-    {
-        if (!$this->getRedirect()) {
-            return $content;
-        }
-        $cachedDocument = new HtmlDocument($content);
-        $meta = $cachedDocument->createElement('meta');
-        $meta->setAttribute('http-equiv', 'Refresh');
-        $meta->setAttribute('content', $this->getRedirect()->getAfterSeconds() . '; url=' . $this->getRedirect()->getTarget());
-        $meta->setAttribute('id', 'meta_redirect');
-        $cachedDocument->head->appendChild($meta);
-
-        return $cachedDocument->saveHTML();
-    }
-
-    private function getRedirect(): ?Redirect
-    {
-        return $this->redirect;
     }
 }
