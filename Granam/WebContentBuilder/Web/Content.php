8e34c81a5da9011965bca178afde2784829ee908
Jaroslav Týc <mail@jaroslavtyc.com>
2019-01-04 17:50:14 +0100

    Html document object can be given

--- Granam/WebContentBuilder/Web/Content.php
+++ Granam/WebContentBuilder/Web/Content.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace Granam\WebContentBuilder\Web;
 
+use Granam\String\StringInterface;
 use Granam\WebContentBuilder\HtmlDocument;
 use Granam\WebContentBuilder\HtmlHelper;
 use Granam\Strict\Object\StrictObject;
 
-class Content extends StrictObject
+class Content extends StrictObject implements StringInterface
 {
     /** @var HtmlHelper */
     private $htmlHelper;
@@ -15,6 +16,8 @@ class Content extends StrictObject
     private $head;
     /** @var Body */
     private $body;
+    /** @var HtmlDocument */
+    private $htmlDocument;
 
     public function __construct(HtmlHelper $htmlHelper, Head $head, Body $body)
     {
@@ -25,15 +28,22 @@ class Content extends StrictObject
 
     public function __toString()
     {
-        return $this->getStringContent();
+        return $this->getValue();
     }
 
-    public function getStringContent(): string
+    public function getValue(): string
     {
-        $content = $this->composeContent();
-        $htmlDocument = $this->buildHtmlDocument($content);
+        return $this->getHtmlDocument()->saveHTML();
+    }
 
-        return $htmlDocument->saveHTML();
+    public function getHtmlDocument(): HtmlDocument
+    {
+        if (!$this->htmlDocument) {
+            $content = $this->composeContent();
+            $this->htmlDocument = $this->buildHtmlDocument($content);
+        }
+
+        return $this->htmlDocument;
     }
 
     private function buildHtmlDocument(string $content): HtmlDocument
